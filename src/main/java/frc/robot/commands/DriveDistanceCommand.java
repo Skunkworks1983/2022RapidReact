407ea893e033835b3e8adb9a7213b6a84fa85b1e
TacoLord346 <TacoLord346@gmail.com>
2022-02-19 10:45:25 -0800

    added some quality changes to DriveDistanceCommand

--- src/main/java/frc/robot/commands/DriveDistanceCommand.java
+++ src/main/java/frc/robot/commands/DriveDistanceCommand.java
@@ -6,10 +6,12 @@ import frc.robot.subsystems.Drivebase;
 
 public class DriveDistanceCommand extends CommandBase
 {
-    private Drivebase drivebase;
-    private double speed;
-    private double distanceFT;
+    private final Drivebase drivebase;
+    private final double speed;
+    private final double distanceFT;
     private double startDistanceFT;
+    private int direction;
+    private double startDegree;
 
     /**
      *
@@ -27,49 +29,29 @@ public class DriveDistanceCommand extends CommandBase
         this.speed = speed;
     }
 
-    /**
-     * The initial subroutine of a command.  Called once when the command is initially scheduled.
-     */
     @Override
     public void initialize()
     {
         startDistanceFT = drivebase.getPosLeft();
+        startDegree = drivebase.getHeading();
 
         if(distanceFT < 0)
         {
-            drivebase.runMotor(speed, speed);
+            direction = 1;
         }
         else
         {
-            drivebase.runMotor(-speed, -speed);
+            direction = -1;
         }
-
+        drivebase.runMotor(speed*direction, speed*direction);
     }
 
-    /**
-     * The main body of a command.  Called repeatedly while the command is scheduled.
-     * (That is, it is called repeatedly until {@link #isFinished()}) returns true.)
-     */
     @Override
     public void execute()
     {
-        System.out.println("Current distance Left: " + drivebase.getPosLeft() + " Feet, Current distance Right: " + drivebase.getPosRight() + " Feet");
+
     }
 
-    /**
-     * <p>
-     * Returns whether this command has finished. Once a command finishes -- indicated by
-     * this method returning true -- the scheduler will call its {@link #end(boolean)} method.
-     * </p><p>
-     * Returning false will result in the command never ending automatically. It may still be
-     * cancelled manually or interrupted by another command. Hard coding this command to always
-     * return true will result in the command executing once and finishing immediately. It is
-     * recommended to use * {@link edu.wpi.first.wpilibj2.command.InstantCommand InstantCommand}
-     * for such an operation.
-     * </p>
-     *
-     * @return whether this command has finished.
-     */
     @Override
     public boolean isFinished()
     {
@@ -83,14 +65,6 @@ public class DriveDistanceCommand extends CommandBase
         }
     }
 
-    /**
-     * The action to take when the command ends. Called when either the command
-     * finishes normally -- that is it is called when {@link #isFinished()} returns
-     * true -- or when  it is interrupted/canceled. This is where you may want to
-     * wrap up loose ends, like shutting off a motor that was being used in the command.
-     *
-     * @param interrupted whether the command was interrupted/canceled
-     */
     @Override
     public void end(boolean interrupted)
     {
