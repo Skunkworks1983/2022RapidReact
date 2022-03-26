f59bf9f6462d7ece80c4424aa934ab23f8025e3e
bella <bellasoutham@gmail.com>
2022-03-26 15:23:21 -0700

    shooter tuning pt 2

--- src/main/java/frc/robot/commands/shooter/ShootCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShootCommand.java
@@ -32,7 +32,7 @@ public class ShootCommand extends CommandBase {
     public void execute()
     {
         SmartDashboard.putNumber("Flywheel speed", shooter.getFlyWheelSpeed());
-        SmartDashboard.putNumber("Liftball spped", shooter.getLiftBallSpeed());
+        SmartDashboard.putNumber("Liftball spe ed", shooter.getLiftBallSpeed());
     }
 
     @Override
